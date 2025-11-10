52d126182ee7397e0e348e06a3b45e0f6cd60ede
IvaBagba <baguena.delabandera.ivan@gmail.com>
2025-11-10 18:38:29 +0100

    v0.01

---
+++ src/main/java/com/eventideCamp/eventideCamp/EventideCampApplication.java
@@ -0,0 +1,13 @@
+package com.eventideCamp.eventideCamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventideCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventideCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventideCamp/eventideCamp/EventideCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventideCamp.eventideCamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventideCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
